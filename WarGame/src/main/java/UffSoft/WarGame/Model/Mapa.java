cfbe552fdb9c5b047bdae5969c61e9e171780851
Leo <leovaladares@outlook.com>
2016-05-14 13:04:15 -0300

    Implementadas as classes do modelo

---
+++ WarGame/src/main/java/UffSoft/WarGame/Model/Mapa.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package UffSoft.WarGame.Model;
+
+import java.util.Set;
+
+/**
+ *
+ * @author Leo
+ */
+public class Mapa 
+{
+    private Set<Continente> continentes;
+}
---
+++ WarGame/src/main/java/UffSoft/WarGame/Model/Territorio.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package UffSoft.WarGame.Model;
+
+import java.util.Set;
+
+/**
+ *
+ * @author Leo
+ */
+public class Territorio
+{
+    private String nome;
+    private Set<Territorio> fronteiras;
+}
