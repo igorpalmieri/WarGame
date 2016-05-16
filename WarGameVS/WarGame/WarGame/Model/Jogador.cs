322c9c2bb25612785822303dea02fb0d654c7d2f
Leo <leovaladares@outlook.com>
2016-05-16 00:13:56 -0300

    EsboÃo das classes do modelo criadas.

---
+++ WarGameVS/WarGame/WarGame/Model/Jogador.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WarGame.Model
+{
+    public class Jogador
+    {
+        private string nome { get; set; }
+        private List<Exercito> exercitos;
+    }
+}
