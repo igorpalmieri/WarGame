f22fb0cc53e41533a6f5b3fc72f18e51c7556e62
Natalia <nataliapipas@gmail.com>
2016-05-21 23:08:37 -0400

    Tela inicial

--- WarGameVS/WarGame/WarGame/Form1.Designer.cs
+++ WarGameVS/WarGame/WarGame/Form1.Designer.cs
@@ -28,12 +28,62 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(97, 177);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(253, 56);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Jogar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Visible = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(85, 239);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(280, 56);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Opcoes";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Visible = false;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Location = new System.Drawing.Point(328, 172);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(469, 333);
+            this.panel1.TabIndex = 2;
+            this.panel1.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::WarGame.Properties.Resources.capa3;
+            this.ClientSize = new System.Drawing.Size(1142, 549);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel panel1;
     }
 }
 
--- WarGameVS/WarGame/WarGame/Form1.cs
+++ WarGameVS/WarGame/WarGame/Form1.cs
@@ -16,5 +16,10 @@ namespace WarGame
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
