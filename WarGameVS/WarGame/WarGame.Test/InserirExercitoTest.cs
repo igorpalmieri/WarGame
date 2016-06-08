f18476c299c63d2c569e730c74867fc0672c2836
Leo <leovaladares@outlook.com>
2016-06-07 21:52:52 -0300

    Faltou o arquivo de teste

---
+++ WarGameVS/WarGame/WarGame.Test/InserirExercitoTest.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WarGame.Model;
+using WarGame.Exceptions;
+using System.Collections.Generic;
+
+namespace WarGame.Test
+{
+    [TestClass]
+    public class InserirExercitoTest
+    {
+        private Jogador _jogadorA, _jogadorB;
+
+        [TestInitialize]
+        public void StartUp()
+        {
+            this._jogadorA = new Jogador("Jogador A", null);
+            this._jogadorB = new Jogador("Jogador B", null);
+        }
+
+        private InserirExercito CriarInsercaoExercito(Jogador jogadorTerritorio, Jogador jogadorExercito)
+        {
+            Territorio territorioDestino = new Territorio("Territorio A");
+            territorioDestino.AddExercito(new Exercito(jogadorTerritorio));
+
+            List<Exercito> exercitosInsercao = new List<Exercito>();
+            exercitosInsercao.Add(new Exercito(jogadorExercito));
+
+            InserirExercito ie = new InserirExercito(territorioDestino, exercitosInsercao);
+            return ie;
+        }
+
+        [TestMethod]
+        public void ValidarInsercaoComandanteTerritorioExercitosIguais()
+        {
+            var ie = this.CriarInsercaoExercito(this._jogadorA, this._jogadorA);
+            Assert.AreEqual(true, ie.validarMovimento());
+        }
+
+        [TestMethod]
+        public void ValidarInsercaoComandanteTerritorioExercitosDiferentes()
+        {
+            var ie = this.CriarInsercaoExercito(this._jogadorA, this._jogadorB);
+            Assert.AreEqual(false, ie.validarMovimento());
+        }
+
+        [TestMethod]
+        public void RealizarInsercaoValida()
+        {
+            var ie = this.CriarInsercaoExercito(this._jogadorA, this._jogadorA);
+            var countExercitosAntesMovimento = ie.TerritorioDestino.CountExercitos();
+
+            Assert.AreEqual(true, ie.validarMovimento());
+
+            ie.realizarMovimento();
+            Assert.IsTrue(ie.TerritorioDestino.CountExercitos() > countExercitosAntesMovimento); 
+        }
+
+        [TestMethod]
+        public void RealizarInsercaoInvalida()
+        {
+            var ie = this.CriarInsercaoExercito(this._jogadorA, this._jogadorB);
+            var countExercitoAntesMovimento = ie.TerritorioDestino.CountExercitos();
+
+            Assert.AreEqual(false, ie.validarMovimento());
+
+            try
+            {
+                ie.realizarMovimento();
+                Assert.Fail();
+            }
+            catch(JogadaInvalidaException ex)
+            {
+                Assert.IsTrue(ie.TerritorioDestino.CountExercitos() == countExercitoAntesMovimento);
+            }
+        }
+    }
+}
